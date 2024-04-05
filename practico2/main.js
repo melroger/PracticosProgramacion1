baea939e1a4f16689b7ed89806d5bbb8e87c7f41
melanierogerqubika <melanie.roger@qubika.com>
2024-04-05 16:51:46 -0300

    paractico 2 - ejercici 30

--- practico2/main.js
+++ practico2/main.js
@@ -1,5 +1,33 @@
 // Ejercicio 29
 
+let contadorPersonaje= Number(0);
+let jugador1Personaje = "&#128520";
+let jugador2Personaje = "&#128520";
+
+document.querySelectorAll(".iconos li").forEach( function (seleccionarPersonaje){
+
+    seleccionarPersonaje.addEventListener("click", function (){
+        
+        contadorPersonaje ++;
+        let iconoSeleccionado = this.textContent;
+
+        this.classList.add("disable");
+
+        if( contadorPersonaje === 1 ){
+            jugador1Personaje = iconoSeleccionado;
+            console.log("jugador1Personaje: " + jugador1Personaje);
+            document.querySelector(".selecionar-jugador").innerHTML = "Jugador 2";
+            
+        }else if( contadorPersonaje >= 2 ){
+            jugador2Personaje = iconoSeleccionado;
+            console.log("jugador2Personaje: " + jugador2Personaje);
+            document.querySelector(".iconos").classList.add("disable");
+        }
+
+    });
+});
+
+
 
 
 let finalizado = false;
@@ -9,24 +37,24 @@ function evaluarPosiciones(jugador){
 
     let casillerosTodos = [];
 
-
     for (let i = 1; i <=9; i++) {
-        let casillero = document.querySelector("#cas" + i).classList.contains(jugador);
+        let casillero = document.querySelector("#cas" + i).textContent;
+
         casillerosTodos.push(casillero); 
     }
 
 
-    if( (casillerosTodos[0] && casillerosTodos[1] && casillerosTodos[2]) || 
-        (casillerosTodos[3] && casillerosTodos[4] && casillerosTodos[5]) || 
-        (casillerosTodos[6] && casillerosTodos[7] && casillerosTodos[8]) || 
+    if( (casillerosTodos[0] === jugador && casillerosTodos[1] === jugador && casillerosTodos[2] === jugador ) || 
+        (casillerosTodos[3] === jugador && casillerosTodos[4] === jugador && casillerosTodos[5] === jugador ) || 
+        (casillerosTodos[6] === jugador && casillerosTodos[7] === jugador && casillerosTodos[8] === jugador ) || 
 
-        (casillerosTodos[0] && casillerosTodos[3] && casillerosTodos[6]) || 
-        (casillerosTodos[1] && casillerosTodos[4] && casillerosTodos[7]) || 
-        (casillerosTodos[2] && casillerosTodos[5] && casillerosTodos[8]) || 
+        (casillerosTodos[0] === jugador  && casillerosTodos[3] === jugador  && casillerosTodos[6] === jugador ) || 
+        (casillerosTodos[1] === jugador  && casillerosTodos[4] === jugador  && casillerosTodos[7] === jugador ) || 
+        (casillerosTodos[2] === jugador  && casillerosTodos[5] === jugador  && casillerosTodos[8] === jugador ) || 
 
-        (casillerosTodos[0] && casillerosTodos[4] && casillerosTodos[8]) || 
-        (casillerosTodos[2] && casillerosTodos[4] && casillerosTodos[6]) ){ 
-            document.querySelector("#ganador").innerHTML= `ganador: ${jugador}`;
+        (casillerosTodos[0] === jugador && casillerosTodos[4] === jugador  && casillerosTodos[8] === jugador ) || 
+        (casillerosTodos[2] === jugador && casillerosTodos[4] === jugador  && casillerosTodos[6] === jugador ) ){ 
+            document.querySelector("#ganador").innerHTML = `ganador: ${jugador}`;
             finalizado = true;
             document.querySelector(".resetgame").classList.add("show");
     }
@@ -39,24 +67,23 @@ let contador= Number(0);
 
 document.querySelectorAll(".casillero").forEach( function (seleccionarCasillero){
     seleccionarCasillero.addEventListener("click", function (){
+
         contador ++;
+        let yaTieneClase = this.textContent;
 
         if(contador % 2 == 0){
-            let yaTieneClase = this.classList.contains("jugador1")
-            if(yaTieneClase){
 
-            }else{
-                this.classList.add("jugador2");
+            if(yaTieneClase === "" ){
+                this.innerHTML = jugador2Personaje;
             }
             
-            evaluarPosiciones("jugador2");
+            evaluarPosiciones(jugador2Personaje);
         }else{
-            let yaTieneClase = this.classList.contains("jugador2")
-            if(yaTieneClase){
-            }else{
-                this.classList.add("jugador1");
+
+            if(yaTieneClase === "" ){
+                this.innerHTML = jugador1Personaje;
             }
-            evaluarPosiciones("jugador1");
+            evaluarPosiciones(jugador1Personaje);
         }
 
 
@@ -64,7 +91,3 @@ document.querySelectorAll(".casillero").forEach( function (seleccionarCasillero)
 });
 
 let reset = document.querySelectorAll(".resetgame");
-
-reset.addEventListener("click", function (){
-
-});

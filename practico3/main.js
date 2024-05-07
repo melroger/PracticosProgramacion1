017c322d0e12c192c403c591e916e9bc0e831625
melanierogerqubika <melanie.roger@qubika.com>
2024-05-07 12:24:21 -0300

    no message

--- practico3/main.js
+++ practico3/main.js
@@ -1,37 +1,162 @@
 
-// ejercicio 20
-
-let a = parseInt(2366);
-let b = parseInt(273);
-let r = parseInt(1);
-let c = parseInt(0);
-let mcd;
-
-let repeticion = 0;
-
-function fmcd(dividendo, divisor, resto){
-    while( resto > 0 ){
-        repeticion ++;
-        c = parseInt(dividendo / divisor);
-        resto = parseInt(dividendo % divisor);
-
-        mcd = divisor;
+// ejercicio 2
+function ejercicio2(numIncicio, numFinal){
     
-        if (resto > 0){
-            dividendo = parseInt(divisor);
-            divisor = parseInt(resto);
-        }
-    
+    let resultado = "";
+    for( let i = numIncicio; i <= numFinal; i ++){
+        resultado = resultado + i + " ";
     }
+    return resultado;
 }
 
-fmcd( a, b, r);
+document.querySelector("#resultado2").innerHTML = ejercicio2(-100, 10);
+
+// ejercicio 4
+function ejercicio4(numIncicio, numFinal){
+    
+    let resultado = "";
+    for( let i = numIncicio; i >= numFinal; i --){
+        resultado = resultado + i + " ";
+    }
+    return resultado;
+}
+
+document.querySelector("#resultado4").innerHTML = ejercicio4(20, -30);
+
+// ejercicio 6
+function ejercicio6(numIncicio, numFinal){
+    
+    let resultado = "";
+    for( let i = numIncicio; i <= numFinal; i ++){
+        let multiplo = i%4;
+        if(multiplo === 0){
+            resultado = resultado + i + " ";
+        }
+    }
+    return resultado;
+}
+
+document.querySelector("#resultado6").innerHTML = ejercicio6(-33, 230);
+
+// ejercicio 8
+document.querySelector("#btn8").addEventListener("click", ejercicio8function)
+let resultado8 = "";
+function ejercicio8function(){
+    let num1 = document.querySelector("#num1").value;
+    let num2 = document.querySelector("#num2").value;
+    
+    if( num1 <= num2 ){
+        
+        for( let i = num1; i <= num2; i ++){
+            resultado8 = resultado8 + i + " ";
+        }
+    }else{
+        for( let i = num1; i >= num2; i --){
+            resultado8 = resultado8 + i + " ";
+        }
+    }
+
+    document.querySelector("#resultado8").innerHTML = resultado8;
+}
 
 
+// ejercicio 10
+document.querySelector("#btn10").addEventListener("click", ejercicio10)
+let resultado10 = parseInt(1);
+
+function ejercicio10(){
+    let num1 = document.querySelector("#ej10num1").value;
+    let num2 = document.querySelector("#ej10num2").value;
+
+    for( let i = num1; i <= num2; i ++){
+        resultado10 =  resultado10*i;
+    }
+    document.querySelector("#resultado10").innerHTML = resultado10;
+}
+
+
+// ejercicio 12
+document.querySelector("#btn12").addEventListener("click", ejercicio12)
+let resultado12 = parseInt(0);
+
+function factorial(n) {
+    if (n < 0) {
+      return 'Factorial no definido para números negativos';
+    }
+    let resultado = 1;
+    for (let i = 1; i <= n; i++) {
+      resultado *= i;
+    }
+    return resultado;
+  }
+
+function ejercicio12(){
+    let num1 = document.querySelector("#ej12num1").value;
+    document.querySelector("#resultado12").innerHTML = factorial(num1);
+}
+
+
+
+// ejercicio 14
+document.querySelector("#btn14").addEventListener("click", ejercicio14);
+let resultado14 = parseInt(0);
+
+function ejercicio14(){
+    let num1 = parseInt(document.querySelector("#ej14num1").value);
+
+    while(num1 > 1){
+        num1 = num1/10;
+        resultado14 ++;
+    }
+    document.querySelector("#resultado14").innerHTML = resultado14;
+}
+
+// ejercicio 16
+document.querySelector("#btn16").addEventListener("click", ejercicio16);
+
+function ejercicio16() {
+    let numInicio = parseInt(document.querySelector("#ej16num1").value);
+    let numFinal = parseInt(document.querySelector("#ej16num2").value);
+
+    function ejercicio16funcion(numInicio, numFinal) {
+        while (numInicio <= numFinal) {
+            if (numInicio % 4 === 0 && numInicio % 6 === 0) {
+                return numInicio;
+            }
+            numInicio++;
+        }
+        return numInicio;
+    }
+
+    document.querySelector("#resultado16").innerHTML = ejercicio16funcion(numInicio, numFinal);
+}
+
+
+// ejercicio 18
+document.querySelector("#btn18").addEventListener("click", ejercicio18);
+
+function ejercicio18() {
+        let numero = parseInt(document.querySelector("#ej18num1").value);
+    
+        if (isNaN(numero) || numero <= 0) {
+            document.getElementById("resultado18").innerHTML = "Por favor, ingresa un número válido mayor que 0";
+            return;
+        }
+    
+        let resultado = "";
+    
+        for (let i = 31; i < 200; i++) {
+            if (i % numero === 0 && i % 2 === 0) {
+                resultado += i + " ";
+            }
+        }
+    
+        // Mostramos los resultados en el párrafo con ID 'resultado'
+        document.querySelector("#resultado18").innerHTML = resultado;
+    
+}
 
 // ejercicio 19
-
-
 document.querySelector("#btn19").addEventListener("click", ejercicio19);
 
     function ejercicio19() {
@@ -90,6 +215,37 @@ document.querySelector("#btn19").addEventListener("click", ejercicio19);
                 }   
             }
     }
+
+// ejercicio 20
+let a = parseInt(2366);
+let b = parseInt(273);
+let r = parseInt(1);
+let c = parseInt(0);
+let mcd;
+
+let repeticion = 0;
+
+function fmcd(dividendo, divisor, resto){
+    while( resto > 0 ){
+        repeticion ++;
+        c = parseInt(dividendo / divisor);
+        resto = parseInt(dividendo % divisor);
+
+        mcd = divisor;
+    
+        if (resto > 0){
+            dividendo = parseInt(divisor);
+            divisor = parseInt(resto);
+        }
+    
+    }
+}
+
+fmcd( a, b, r);
+
+
+
+
 
 
 
--- practico4/main.js
+++ practico4/main.js
@@ -1,10 +1,137 @@
 // Ejercicio 2
+document.querySelector("#btnS2").addEventListener("click", ejercicioS2);
+function ejercicioS2() {
+    let inputS2 = String(document.querySelector("#inputS2").value.trim());
+    let bocalesContador = parseInt(0);
+
+    for(let i = 0; i <= inputS2.length; i ++){
+        if( inputS2[i] === "a" || inputS2[i] === "e" || inputS2[i] === "i"  || inputS2[i] === "o"  || inputS2[i] === "u" ){
+            bocalesContador ++;
+        }
+    }
+    document.querySelector("#resultado2").innerHTML = bocalesContador;
+}
+
+
+// Ejercicio 4
+document.querySelector("#btnS4").addEventListener("click", ejercicioS4);
+
+function ejercicioS4() {
+    let inputS4 = String(document.querySelector("#inputS4").value.trim());
+    let inputS4segundo = String(document.querySelector("#inputS4segundo").value.trim());
+    let incluyeS4 = inputS4.includes(inputS4segundo);
+
+    if(incluyeS4){
+        document.querySelector("#resultado4").innerHTML = "Incluye";
+    }else{
+        document.querySelector("#resultado4").innerHTML = "No incluye";
+    }
+    
+}
+
+
+// Ejercicio 6
+document.querySelector("#btnS6").addEventListener("click", ejercicioS6);
+
+function ejercicioS6() {
+    let inputS6 = String(document.querySelector("#inputS6").value.trim());
+    let contadorLetrasS6 = inputS6.length;
+    let bocalesContadorS6 = parseInt(0);
+    let consonantesContadorS6 = parseInt(0);
+
+    if(contadorLetrasS6 >= 10){
+        document.querySelector("#resultado6").innerHTML = "ok";
+        for(let i = 0; i < inputS6.length; i ++){
+            if( inputS6[i] === "a" || inputS6[i] === "e" || inputS6[i] === "i"  || inputS6[i] === "o"  || inputS6[i] === "u" ){
+                bocalesContadorS6 ++;
+            }else{
+                consonantesContadorS6 ++;
+            }
+        }
+        if(bocalesContadorS6 >= consonantesContadorS6){
+            document.querySelector("#resultado6").innerHTML = bocalesContadorS6 - consonantesContadorS6;
+        }else{
+            document.querySelector("#resultado6").innerHTML = consonantesContadorS6 - bocalesContadorS6;
+        }
+
+        
+    }else{
+        document.querySelector("#resultado6").innerHTML = "No es suficiente texto";
+    }
+   
+}
+
+// Ejercicio 8
+document.querySelector("#btnS8").addEventListener("click", ejercicioS8);
+
+function ejercicioS8() {
+
+   
+}
+
+
+// Ejercicio 10
+document.querySelector("#btnS10").addEventListener("click", ejercicioS10);
+
+function ejercicioS10() {
+    let inputS10 = document.querySelector("#inputS10").value.trim();
+    let resutadoS10 = parseInt(0);
+    let letraS10;
+
+    for(let i = 0; i < inputS10.length; i ++){
+        letraS10 = inputS10[i];
+        if (!isNaN(letraS10)){
+            resutadoS10 = resutadoS10 + parseInt(letraS10);
+        }
+    }
+    document.querySelector("#resultado10").innerHTML = resutadoS10;
+}
+
+
+// Ejercicio 12
+document.querySelector("#btnS12").addEventListener("click", ejercicioS12);
+
+function ejercicioS12() {
+    let inputS12 = String(document.querySelector("#inputS12").value);
+    let resultado12 = "";
+
+    for(let i = inputS12.length - 1; i >= 0; i--) {
+        resultado12 = resultado12 + inputS12[i];
+    }
+    document.querySelector("#resultado12").innerHTML = resultado12;
+}
+
+// Ejercicio 14
+document.querySelector("#btnS14").addEventListener("click", ejercicioS14);
+
+function ejercicioS14() {
+    let inputS14 = document.querySelector("#inputS14").value.trim();
+
+    let resultado14 ="";
+
+    if(inputS14.length !== 7){
+        resultado14 = "matricula invalida";
+    }else{
+        if( inputS14[0] !== NaN){
+            console.log("es un numero");
+            console.log(inputS14[0]);
+        }else{
+            console.log("NO es un numero");
+            console.log(inputS14[0]);
+        }
+
+    }
+
+    document.querySelector("#resultado14").innerHTML = resultado14;
+}
+
+// Ejercicio 16
 
 document.querySelector("#btnS16").addEventListener("click", ejercicioS16);
 
     function ejercicioS16() {
 
-        let inputS16 = (document.querySelector("#inputS16").value);
+        let inputS16 = document.querySelector("#inputS16").value;
         let textS16 = String(inputS16).toLowerCase();
 
         let textS16Sanetized = String();
@@ -49,3 +176,28 @@ document.querySelector("#btnS16").addEventListener("click", ejercicioS16);
         }
 
     }
+
+
+
+// Ejercicio 10 funciones
+
+document.querySelector("#btnF10").addEventListener("click", ejercicioF10);
+
+function ejercicioF10() {
+    let horas = document.querySelector("#horas").value;
+    let materiales = document.querySelector("#materiales").value;
+
+
+    /////////////// esto evalua si es un numero y ademas positivo
+    function esNumero(valor){
+        if(!isNaN(valor) && valor >= 0) {
+            return true
+        }else{
+            return false
+        }
+    }
+    let valorNumricoHoras = esNumero(horas);
+    let valorNumricoMateriales = esNumero(materiales);
+    console.log("valorNumricoHoras: " + valorNumricoHoras + " valorNumricoMateriales: " + valorNumricoMateriales);
+    
+}
